cd9f853fbec49811ce21b28fe592fb32dea98faf
Salamando <salamandog@yandex.ru>
2024-05-20 21:52:29 +0300

    Домашнее задание к занятию «Препроцессор и макросы»

---
+++ 07/01/cppm-homeworks_07_01/cppm-homeworks_07_01/cppm-homeworks_07_01.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+#define MODE 1
+
+#ifndef MODE
+#error Необходимои определить MODE
+#endif
+
+#if MODE == 1
+double add(double x, double y) {
+	return x + y;
+}
+#endif
+
+int main() {
+
+#if MODE == 0
+	std::cout << "Работаю в режиме тренировки" << std::endl;
+#elif MODE == 1
+	double x, y;
+	std::cout << "Работаю в боевом режиме" << std::endl;
+
+	std::cout << "Введите число 1: ";
+	std::cin >> x;
+
+	std::cout << "Введите число 2: ";
+	std::cin >> y;
+
+	std::cout << "Результат сложения: " << add(x, y);
+
+#else
+	std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+#endif
+
+	return 0;
+}
