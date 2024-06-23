c1c93387a2121fe294ef5239c9373ff272e7a633
Salamando <salamandog@yandex.ru>
2024-06-23 21:59:50 +0300

    Домашнее задание к занятию «Динамическое программирование и “жадность”»

---
+++ algocpp/03/algocpp03_01/algocpp03_01/algocpp03_01.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int fib(int n) {
+    if (n <= 1)
+        return n;
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main() {
+    int n;
+    std::cout << "Введите число: ";
+    std::cin >> n;
+    std::cout << "Числа Фибоначчи: ";
+    for (int i = 0; i <= n; i++) {
+        std::cout << fib(i) << " ";
+    }
+    return 0;
+}
---
+++ algocpp/03/algocpp03_02/algocpp03_02/algocpp03_02.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+int fib(int n, int* cacheArray) {
+    if (n <= 1)
+        return n;
+
+    if (cacheArray[n] != -1) {
+        return cacheArray[n];
+    }
+
+    cacheArray[n] = fib(n - 1, cacheArray) + fib(n - 2, cacheArray);
+    return cacheArray[n];
+}
+
+int main() {
+    int n;
+    std::cout << "Введите число: ";
+    std::cin >> n;
+
+    int* cacheArray = new int[n + 1];
+    for (int i = 0; i <= n; i++) {
+        cacheArray[i] = -1;
+    }
+
+    std::cout << "Числа Фибоначчи: ";
+    for (int i = 0; i <= n; i++) {
+        std::cout << fib(i, cacheArray) << " ";
+    }
+
+    delete[] cacheArray;
+
+    return 0;
+}
