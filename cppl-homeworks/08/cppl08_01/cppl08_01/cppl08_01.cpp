52edaf2a35939c64ed979287bf4638aea618024e
Salamando <salamandog@yandex.ru>
2024-08-18 14:00:29 +0300

    Домашнее задание «STL. Часть 2»

---
+++ cppl-homeworks/08/cppl08_01/cppl08_01/cppl08_01.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template<typename T>
+void removeDuplicates(std::vector<T>& vec) {
+    std::sort(vec.begin(), vec.end());
+
+    auto last = std::unique(vec.begin(), vec.end());
+
+    vec.erase(last, vec.end());
+}
+
+int main() {
+    std::vector<int> vec = { 1, 1, 2, 5, 6, 1, 2, 4 };
+
+    std::cout << "[IN]: ";
+    for (const auto& v : vec) {
+        std::cout << v << " ";
+    }
+
+    std::cout << std::endl;
+
+    removeDuplicates(vec);
+
+    std::cout << "[OUT]: ";
+    for (const auto& v : vec) {
+        std::cout << v << " ";
+    }
+
+    return 0;
+}
---
+++ cppl-homeworks/08/cppl08_02/cppl08_02/cppl08_02.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <iterator>
+#include <set>
+#include <vector>
+#include <list>
+
+template <typename T>
+void print_container(const T& test_container) {
+    for (auto it = test_container.begin(); it != test_container.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set);
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list);
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector);
+
+    return 0;
+}
