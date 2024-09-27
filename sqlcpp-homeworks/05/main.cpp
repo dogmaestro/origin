31393711a3080c739ff978e7b5ecf03c161b4473
Salamando <salamandog@yandex.ru>
2024-09-27 20:14:43 +0300

    Изменена обработка исключений, вывод на консоль перенесен в main

--- sqlcpp-homeworks/05/main.cpp
+++ sqlcpp-homeworks/05/main.cpp
@@ -3,159 +3,121 @@
 #include <string>
 // #include <Windows.h>
 #include <vector>
+#include <optional>
+
+struct ClientData
+{
+	int id;
+	std::string name;
+	std::string last_name;
+	std::string email;
+};
 
 class ClientDBManager
 {
 public:
-	ClientDBManager(const std::string &connection_str);
+	ClientDBManager();
+	void initConnection(const std::string &connection_str);
 	void createDatabaseStructure();
 	int addClient(const std::string &name, const std::string &last_name, const std::string &email, const std::vector<std::string> &phone_numbers);
 	void addPhoneToClient(std::string &phone_number, const int &client_id);
 	void updateClient(const std::string &name, const std::string &last_name, const std::string &email, const std::vector<std::string> &phone_numbers, const int &client_id);
 	void deletePhoneToClient(std::string &phone_number, const int &client_id);
 	void deleteClient(const int &client_id);
-	void findClient(const std::string &name, const std::string &last_name, const std::string &email, const std::string &phone_number);
+	std::optional<ClientData> findClient(const std::string &name, const std::string &last_name, const std::string &email, const std::string &phone_number);
+
 private:
 	std::unique_ptr<pqxx::connection> conn;
 };
 
-ClientDBManager::ClientDBManager(const std::string &connection_str)
+ClientDBManager::ClientDBManager() {};
+
+void ClientDBManager::initConnection(const std::string &connection_str)
 {
-	try
+	conn = std::make_unique<pqxx::connection>(connection_str);
+	if (!conn->is_open())
 	{
-		conn = std::make_unique<pqxx::connection>(connection_str);
-		if (!conn->is_open())
-		{
-			std::cerr << "Ошибка соединения с БД" << std::endl;
-			exit(1);
-		}
-	}
-	catch (const std::exception &e)
-	{
-		std::cerr << e.what() << std::endl;
-		exit(1);
+		throw "Ошибка соединения с БД";
 	}
 };
 
 void ClientDBManager::createDatabaseStructure()
 {
-	try
-	{
-		pqxx::transaction t(*conn);
+	pqxx::transaction t(*conn);
 
-		t.exec(
-			"CREATE TABLE IF NOT EXISTS clients ( "
-			"id SERIAL PRIMARY KEY, "
-			"name VARCHAR(100) NOT NULL, "
-			"last_name VARCHAR(100) NOT NULL, "
-			"email VARCHAR(100) NOT NULL UNIQUE);");
+	t.exec(
+		"CREATE TABLE IF NOT EXISTS clients ( "
+		"id SERIAL PRIMARY KEY, "
+		"name VARCHAR(100) NOT NULL, "
+		"last_name VARCHAR(100) NOT NULL, "
+		"email VARCHAR(100) NOT NULL UNIQUE);");
 
-		t.exec(
-			"CREATE TABLE IF NOT EXISTS phones ("
-			"id SERIAL PRIMARY KEY, "
-			"client_id INTEGER NOT NULL, "
-			"phone_number VARCHAR(100) NOT NULL, "
-			"FOREIGN KEY (client_id) REFERENCES clients(id) ON DELETE CASCADE);");
+	t.exec(
+		"CREATE TABLE IF NOT EXISTS phones ("
+		"id SERIAL PRIMARY KEY, "
+		"client_id INTEGER NOT NULL, "
+		"phone_number VARCHAR(100) NOT NULL, "
+		"FOREIGN KEY (client_id) REFERENCES clients(id) ON DELETE CASCADE);");
 
-		t.commit();
-	}
-	catch (const std::exception &e)
-	{
-		std::cerr << e.what() << std::endl;
-		exit(1);
-	};
+	t.commit();
 };
 
 int ClientDBManager::addClient(const std::string &name, const std::string &last_name, const std::string &email, const std::vector<std::string> &phone_numbers)
 {
-	try
+	pqxx::transaction t(*conn);
+
+	pqxx::result result = t.exec("INSERT INTO clients (name, last_name, email) VALUES (" +
+								 t.quote(name) + ", " +
+								 t.quote(last_name) + ", " +
+								 t.quote(email) +
+								 ") RETURNING id;");
+	int client_id = result[0][0].as<int>();
+
+	for (const auto &phone : phone_numbers)
 	{
-		pqxx::transaction t(*conn);
-
-		pqxx::result result = t.exec("INSERT INTO clients (name, last_name, email) VALUES (" +
-									 t.quote(name) + ", " +
-									 t.quote(last_name) + ", " +
-									 t.quote(email) +
-									 ") RETURNING id;");
-		int client_id = result[0][0].as<int>();
-
-		for (const auto &phone : phone_numbers)
-		{
-			t.exec("INSERT INTO phones (client_id, phone_number) VALUES (" +
-				   t.quote(client_id) + ", " + t.quote(phone) + ");");
-		}
-
-		t.commit();
-		return client_id;
+		t.exec("INSERT INTO phones (client_id, phone_number) VALUES (" +
+			   t.quote(client_id) + ", " + t.quote(phone) + ");");
 	}
-	catch (const std::exception &e)
-	{
-		std::cerr << "Ошибка добавления клиента: " << e.what() << std::endl;
-		exit(1);
-	}
+
+	t.commit();
+	return client_id;
 }
 
 void ClientDBManager::updateClient(const std::string &new_name, const std::string &new_last_name, const std::string &new_email, const std::vector<std::string> &new_phone_numbers, const int &client_id)
 {
-	try
+	pqxx::transaction t(*conn);
+
+	pqxx::result result = t.exec("UPDATE clients SET name = " + t.quote(new_name) +
+								 ", last_name = " + t.quote(new_last_name) +
+								 ", email = " + t.quote(new_email) +
+								 " WHERE id = " + t.quote(client_id));
+
+	t.exec("DELETE FROM phones WHERE id = " + t.quote(client_id));
+
+	for (const auto &phone : new_phone_numbers)
 	{
-		pqxx::transaction t(*conn);
-
-		pqxx::result result = t.exec("UPDATE clients SET name = " + t.quote(new_name) +
-									 ", last_name = " + t.quote(new_last_name) +
-									 ", email = " + t.quote(new_email) +
-									 " WHERE id = " + t.quote(client_id));
-
-		t.exec("DELETE FROM phones WHERE id = " + t.quote(client_id));
-
-		for (const auto &phone : new_phone_numbers)
-		{
-			t.exec("INSERT INTO phones (client_id, phone_number) VALUES (" +
-				   t.quote(client_id) + ", " + t.quote(phone) + ");");
-		}
-
-		t.commit();
+		t.exec("INSERT INTO phones (client_id, phone_number) VALUES (" +
+			   t.quote(client_id) + ", " + t.quote(phone) + ");");
 	}
-	catch (const std::exception &e)
-	{
-		std::cerr << "Ошибка изменения клиента: " << e.what() << std::endl;
-		exit(1);
-	}
+
+	t.commit();
 }
 
 void ClientDBManager::addPhoneToClient(std::string &phone_number, const int &client_id)
 {
-	try
-	{
-		pqxx::transaction t(*conn);
+	pqxx::transaction t(*conn);
 
-		t.exec("INSERT INTO phones (client_id, phone_number) VALUES (" +
-			   t.quote(client_id) + ", " + t.quote(phone_number) + ");");
-		t.commit();
-	}
-
-	catch (const std::exception &e)
-	{
-		std::cerr << "Ошибка добавления телефона: " << e.what() << std::endl;
-		exit(1);
-	}
+	t.exec("INSERT INTO phones (client_id, phone_number) VALUES (" +
+		   t.quote(client_id) + ", " + t.quote(phone_number) + ");");
+	t.commit();
 }
 
 void ClientDBManager::deletePhoneToClient(std::string &phone_number, const int &client_id)
 {
-	try
-	{
-		pqxx::transaction t(*conn);
+	pqxx::transaction t(*conn);
 
-		t.exec("DELETE FROM phones WHERE client_id = " + t.quote(client_id) + " AND phone_number = " + t.quote(phone_number) + ";");
-		t.commit();
-	}
-
-	catch (const std::exception &e)
-	{
-		std::cerr << "Ошибка удаления телефона: " << e.what() << std::endl;
-		exit(1);
-	}
+	t.exec("DELETE FROM phones WHERE client_id = " + t.quote(client_id) + " AND phone_number = " + t.quote(phone_number) + ";");
+	t.commit();
 }
 
 void ClientDBManager::deleteClient(const int &client_id)
@@ -175,74 +137,82 @@ void ClientDBManager::deleteClient(const int &client_id)
 	}
 }
 
-void ClientDBManager::findClient(const std::string &name, const std::string &last_name, const std::string &email, const std::string &phone_number)
+std::optional<ClientData> ClientDBManager::findClient(const std::string &name, const std::string &last_name, const std::string &email, const std::string &phone_number)
 {
-	try
+	std::string sql;
+	pqxx::transaction t(*conn);
+
+	if (phone_number.empty())
 	{
-		std::string sql;
-		pqxx::transaction t(*conn);
-
-		if (phone_number.empty())
-		{
-			sql = "SELECT * FROM clients c WHERE ";
-		}
-		else
-		{
-			sql = "SELECT * FROM clients c JOIN phones p ON c.id = p.client_id WHERE ";
-		}
-
-		bool useAND = false;
-
-		if (!name.empty())
-		{
-			sql += "c.name = " + t.quote(name);
-			useAND = true;
-		}
-
-		if (!last_name.empty())
-		{
-			if (useAND)
-				sql += " AND ";
-			sql += "c.last_name = " + t.quote(last_name);
-			useAND = true;
-		}
-
-		if (!email.empty())
-		{
-			if (useAND)
-				sql += " AND ";
-			sql += "c.email = " + t.quote(email);
-			useAND = true;
-		}
-
-		if (!phone_number.empty())
-		{
-			if (useAND)
-				sql += " AND ";
-			sql += "p.phone_number = " + t.quote(phone_number);
-		}
-
-		pqxx::result res = t.exec(sql);
-
-		if (res.empty())
-		{
-			std::cout << "Клиент не найден" << std::endl;
-		}
-		else
-		{
-			for (const auto &row : res)
-			{
-				std::cout << "Клиент: "
-						  << "имя = " << row["name"].as<std::string>() << ", "
-						  << "фамилия = " << row["last_name"].as<std::string>() << ", "
-						  << "Email = " << row["email"].as<std::string>() << std::endl;
-			}
-		}
+		sql = "SELECT * FROM clients c WHERE ";
 	}
-	catch (const std::exception &e)
+	else
 	{
-		std::cerr << "Ошибка поиска клиента: " << e.what() << std::endl;
-		exit(1);
+		sql = "SELECT * FROM clients c JOIN phones p ON c.id = p.client_id WHERE ";
+	}
+
+	bool useAND = false;
+
+	if (!name.empty())
+	{
+		sql += "c.name = " + t.quote(name);
+		useAND = true;
+	}
+
+	if (!last_name.empty())
+	{
+		if (useAND)
+			sql += " AND ";
+		sql += "c.last_name = " + t.quote(last_name);
+		useAND = true;
+	}
+
+	if (!email.empty())
+	{
+		if (useAND)
+			sql += " AND ";
+		sql += "c.email = " + t.quote(email);
+		useAND = true;
+	}
+
+	if (!phone_number.empty())
+	{
+		if (useAND)
+			sql += " AND ";
+		sql += "p.phone_number = " + t.quote(phone_number);
+	}
+
+	pqxx::result res = t.exec(sql);
+
+	if (res.empty())
+	{
+		return std::nullopt;
+	}
+	else
+	{
+		ClientData clientData;
+		clientData.id = res[0]["id"].as<int>();						  
+		clientData.name = res[0]["name"].as<std::string>();			  
+		clientData.last_name = res[0]["last_name"].as<std::string>(); 
+		clientData.email = res[0]["email"].as<std::string>();		  
+
+		return clientData;
+	}
+}
+
+void printClient(std::optional<ClientData> clientData)
+{
+	if (clientData)
+	{
+		const auto &client = *clientData;
+		std::cout << "Клиент: "
+				  << "имя = " << client.name << ", "
+				  << "фамилия = " << client.last_name << ", "
+				  << "Email = " << client.email << std::endl;
+	}
+	else
+	{
+		std::cout << "Клиент не найден" << std::endl;
 	}
 }
 
@@ -252,50 +222,137 @@ int main()
 	// setlocale(LC_ALL, "ru_RU.UTF-8");
 
 	std::string connection_str = "dbname=postgres user=postgres password=password hostaddr=127.0.0.1 port=5432";
+	std::string name;
+	std::string last_name;
+	std::string email;
+	std::string phone_number;
+	std::vector<std::string> phone_numbers;
+	ClientDBManager manager;
+	int client_id1;
+	int client_id2;
 
-	ClientDBManager manager(connection_str);
+	try
+	{
+		manager.initConnection(connection_str);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Ошибка соединения: " << e.what() << std::endl;
+		return -1;
+	};
 
-	manager.createDatabaseStructure();
+	try
+	{
+		manager.createDatabaseStructure();
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Ошибка создания таблиц: " << e.what() << std::endl;
+		return -2;
+	};
 
-	std::string name = "Иван";
-	std::string last_name = "Иванов";
-	std::string email = "ivanov@mail.com";
-	std::vector<std::string> phone_numbers = {"123-456-789", "111-222-333"};
-	int client_id1 = manager.addClient(name, last_name, email, phone_numbers);
+	try
+	{
+		name = "Иван";
+		last_name = "Иванов";
+		email = "ivanov@mail.com";
+		phone_numbers = {"123-456-789", "111-222-333"};
+		client_id1 = manager.addClient(name, last_name, email, phone_numbers);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Ошибка добавления клиента: " << e.what() << std::endl;
+	}
 
-	std::string phone_number = "333-444-555";
-	manager.addPhoneToClient(phone_number, client_id1);
+	try
+	{
+		phone_number = "333-444-555";
+		manager.addPhoneToClient(phone_number, client_id1);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Ошибка добавления телефона: " << e.what() << std::endl;
+	}
 
-	name = "Иван2";
-	last_name = "Иванов2";
-	email = "ivanov2@mail.com";
-	phone_numbers = {"777-765-098", "567-432-742"};
-	int client_id2 = manager.addClient(name, last_name, email, phone_numbers);
+	try
+	{
+		name = "Иван2";
+		last_name = "Иванов2";
+		email = "ivanov2@mail.com";
+		phone_numbers = {"777-765-098", "567-432-742"};
+		client_id2 = manager.addClient(name, last_name, email, phone_numbers);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Ошибка добавления клиента: " << e.what() << std::endl;
+	}
 
-	phone_number = "333-444-555";
-	manager.addPhoneToClient(phone_number, client_id2);
+	try
+	{
+		phone_number = "333-444-555";
+		manager.addPhoneToClient(phone_number, client_id2);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Ошибка добавления телефона: " << e.what() << std::endl;
+	}
 
-	name = "Петр";
-	last_name = "Петров";
-	email = "petrov@mail.com";
-	phone_numbers = {"444-555-666", "777-888-999"};
-	manager.updateClient(name, last_name, email, phone_numbers, client_id1);
+	try
+	{
+		name = "Петр";
+		last_name = "Петров";
+		email = "petrov@mail.com";
+		phone_numbers = {"444-555-666", "777-888-999"};
+		manager.updateClient(name, last_name, email, phone_numbers, client_id1);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Ошибка изменения клиента: " << e.what() << std::endl;
+	}
 
-	phone_number = "777-888-999";
-	manager.deletePhoneToClient(phone_number, client_id1);
+	try
+	{
+		phone_number = "777-888-999";
+		manager.deletePhoneToClient(phone_number, client_id1);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Ошибка удаления телефона: " << e.what() << std::endl;
+	}
 
-	manager.deleteClient(client_id2);
+	try
+	{
+		manager.deleteClient(client_id2);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Ошибка удаления клиента: " << e.what() << std::endl;
+	}
 
-	name = "Иван";
-	last_name = "Иванов";
-	email = "ivanov@mail.com";
-	manager.findClient(name, last_name, email, "");
+	try
+	{
+		name = "Иван";
+		last_name = "Иванов";
+		email = "ivanov@mail.com";
+		printClient(manager.findClient(name, last_name, email, ""));
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Ошибка поиска клиента: " << e.what() << std::endl;
+	}
 
-	name = "Петр";
-	last_name = "Петров";
-	email = "petrov@mail.com";
-	phone_number = "444-555-666";
-	manager.findClient(name, last_name, email, phone_number);
+	try
+	{
+		name = "Петр";
+		last_name = "Петров";
+		email = "petrov@mail.com";
+		phone_number = "444-555-666";
+		printClient(manager.findClient(name, last_name, email, phone_number));
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Ошибка поиска клиента: " << e.what() << std::endl;
+	}
 
 	return 0;
 }
